4b039e6a936bfdf5baeea53e3c2601796bb1c4b9
unknown <meltanan@uncc.edu>
2017-11-16 19:27:45 -0500

    Added login, signin, and signup pages

---
+++ logIn.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mohamed
+ * Date: 11/16/2017
+ * Time: 7:19 PM
+ */
+
+
+
+?>
+
+<html>
+<form method="post">
+    <input type="text" >
+</form>
+</html>
+
---
+++ signUp.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mohamed
+ * Date: 11/16/2017
+ * Time: 7:19 PM
+ */
