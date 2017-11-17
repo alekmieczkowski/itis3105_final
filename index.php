9ce594043c9416e08ebce9a0a0aec01287fc2cb1
unknown <meltanan@uncc.edu>
2017-11-16 19:45:46 -0500

    Organized everything, added git ignore

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
 <?php
-    //redirect
-    header("Location: site/index.php");
+/*
+*
+ * Created by PhpStorm.
+ * User: Mohamed
+ * Date: 11/16/2017
+ * Time: 7:08 PM
+ */
+
+//redirect to site/index.php
+Header("Location: site/index.php");
+
 ?>
+
+<html>
+
+<form action="user/"
+    <button name="logIn" type="submit" value="LogIn">LogIn</button>
+</form>
+
+<form action="itis3105_final/signUp.php" >
+    <button name="logIn" type="submit" value="LogIn">SignUp</button>
+</form>
+
+
+<button name="logIn" value="LogIn">Show Events</button>
+
+</html>
